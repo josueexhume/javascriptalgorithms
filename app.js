e31ec2353fb9ff457a1a46c473af1b4c4a0125d0
Josue Exhume <exhumejosue@gmail.com>
2018-08-05 18:47:50 -0400

    added is Palindrome Algorithm

--- app.js
+++ app.js
@@ -57,3 +57,15 @@ function harmlessRansomNote(noteText, magazineText) {
 //harmlessRansomNote('this is a secret note for you from a secret admirer', 'puerto rico is a place of great wonder and excitement it has many secret waterfall locatoins that i am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip i have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places i have ever visited');
 
 })();
+
+
+var isPalindromeAlgorithm = (function(){
+  // a palindrome is a word or sentence that's spelled the sames foward and backward, like racecar or madam, i'm adam
+  function isPalindrome (string){
+    var stringFormatted = string.replace(/['|.|\s|,]/g, '');
+    var stringReversed = string.replace(/['|.|\s|,]/g, '').split("").reverse().join("");
+
+    return stringFormatted === stringReversed;
+  }
+  console.log(isPalindrome("madam i'm adam"));
+})();
