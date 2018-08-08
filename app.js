f13e6cadc5f74694fd38b420099d335ddaa6aabc
Josue Exhume <exhumejosue@gmail.com>
2018-08-07 21:56:41 -0400

    added caesar cipher algorithm

--- app.js
+++ app.js
@@ -88,3 +88,48 @@ var isPalindromeAlgorithm = (function () {
   //console.log(isPalindrome("madam i'm adam", false));
 
 })();
+
+
+var caesarCipherAlgorithm = (function () {
+
+  function caesarCipher(str, num) {
+    num = num % 26;
+
+    var lowerCaseString = str.toLowerCase();
+    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+    var newString = "";
+    for (let i = 0; i < lowerCaseString.length; i++) {
+      var currentLetter = lowerCaseString[i];
+      if (currentLetter === ' ') {
+        newString += currentLetter;
+        continue;
+      }
+
+      var currentIndex = alphabet.indexOf(currentLetter);
+      var newIndex = currentIndex + num;
+
+      if (newIndex > 25) {
+        newIndex = newIndex - 26;
+      }
+
+      if (newIndex < 0) {
+        newIndex = newIndex + 26;
+      }
+
+      if (str[i] === str[i].toUpperCase()) {
+        newString += alphabet[newIndex].toUpperCase();
+      } else {
+        newString += alphabet[newIndex];
+      }
+    }
+
+    return newString;
+  }
+
+  //uncomment to run
+  // console.log(caesarCipher("Zoo Keeper", 2));
+  // console.log(caesarCipher("Big Car", -16));
+  // console.log(caesarCipher("Javascript", -900));
+
+})();
