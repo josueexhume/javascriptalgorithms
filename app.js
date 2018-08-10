35c7e99e4f6a62249a344d96beae34742d4a63e7
Josue Exhume <exhumejosue@gmail.com>
2018-08-09 21:27:11 -0400

    added reverse array in place algorithm

--- app.js
+++ app.js
@@ -164,3 +164,22 @@ var reverseWordsAlgorithm = (function () {
   //uncomment to run
   //console.log(reverseString("this is a string of words"));
 })();
+
+
+var reverseArrayInPlaceAlgorithm = (function () {
+
+  //takes in an array and reverse the array
+  function reverseArrayInPlace(arr) {
+    for (let i = 0; i < arr.length / 2; i++) { // o(n/2) since we only do half the length
+      var itemStart = arr[i];
+      var itemEnd = arr[arr.length - (i + 1)];
+      arr[i] = itemEnd;
+      arr[arr.length - (i + 1)] = itemStart;
+    }
+
+    return arr;
+  }
+
+  //uncomment to run
+  //console.log(reverseArrayInPlace([1,2,3,4,5,6,7,8,9,10]));
+})();
