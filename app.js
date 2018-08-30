db8ee78b56999fe8da68a5c25414215d4e37fde9
Josue Exhume <exhumejosue@gmail.com>
2018-08-29 20:03:07 -0400

    added factorial algorithm

--- app.js
+++ app.js
@@ -271,3 +271,41 @@ var twoSumAlgorithm = (function () {
   //uncomment to run
   //console.log(twoSum([1,6,4,5,3,3], 7));
 })();
+
+var twoSumAlgorithm = (function () {
+
+  //returns every pair of numbers that adds up to a given sum
+  function twoSum(numArray, sum) {
+    var result = [];
+    var hashTable = [];
+      for (let i = 0; i < numArray.length; i++) {
+        var currNum = numArray[i];
+        var counterPart = sum - currNum;
+        if(hashTable.indexOf(counterPart) > -1){
+          result.push([currNum, counterPart]);
+        } 
+        hashTable.push(numArray[i]);
+      }
+
+    return result;
+  }
+
+  //uncomment to run
+  //console.log(twoSum([1,6,4,5,3,3], 7));
+})();
+
+
+var factorialAlgorithm = (function () {
+  function factorial(num) {
+    if(num === 1){
+      return num;
+    } else {
+      return num * factorial( num - 1);
+    }
+  }
+
+  //uncomment to run
+  //console.log(factorial(5));
+
+  })();
+
