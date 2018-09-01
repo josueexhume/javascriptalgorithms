0f43b720dcfa4281b156df3905f35eb5060503c9
Josue Exhume <exhumejosue@gmail.com>
2018-09-01 11:55:49 -0400

    added fibonacci algorithm

---
+++ Algorithm/fibonacci.js
@@ -0,0 +1,16 @@
+var fibonacciAlgorithm = (function () {
+
+  //generates a sequence of number up to the nth position and returns that last number.
+  //this version of the algoritms time complexity is O(2^N) which is very in efficient
+  function fibonacci(position) {
+    //this was hard to wrap my head around but if you visualize it as a tree, it becomes easier to 
+    //understand
+    if(position  < 3){
+      return 1;
+    } else {
+      return fibonacci(position - 1) + fibonacci(position - 2);
+    }
+  }
+
+  console.log(fibonacci(40));
+})();
