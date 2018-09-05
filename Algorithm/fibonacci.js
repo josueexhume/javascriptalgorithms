2a81327f38c3186d6e4a41009bf6c82a9a112cf4
Josue Exhume <exhumejosue@gmail.com>
2018-09-04 22:13:13 -0400

    fixed typo

--- Algorithm/fibonacci.js
+++ Algorithm/fibonacci.js
@@ -1,7 +1,7 @@
 var fibonacciAlgorithm = (function () {
 
   //generates a sequence of number up to the nth position and returns that last number.
-  //this version of the algoritms time complexity is O(2^N) which is very in efficient
+  //this version of the algoritms time complexity is O(2^N) which is very inefficient
   function fibonacci(position) {
     //this was hard to wrap my head around but if you visualize it as a tree, it becomes easier to 
     //understand
@@ -12,5 +12,5 @@ var fibonacciAlgorithm = (function () {
     }
   }
 
-  console.log(fibonacci(40));
+  console.log(fibonacci(4));
 })();
